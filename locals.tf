fd8b9b89be9be5884474f052481555e95856af49
andrew reichek <andrewreichek@andrews-iMac.local>
2024-08-14 10:04:06 -0500

    new local.tf

--- locals.tf
+++ locals.tf
@@ -1,7 +1,7 @@
 locals {
   ami          = "ami-0ae8f15ae66fe8cda"  # Replace with your actual AMI ID
   instancetype = "t3a.medium"               # Replace with your desired instance type
-  keyname      = "your pem key no extension"          # Replace with your actual key name
+  key_name      = "your pem key no extension"          # Replace with your actual key name
   k3s_master_sec_group_id = aws_security_group.k3s-master-sec-group.id
   k3s_worker_sec_group_id = aws_security_group.k3s-worker-sec-group.id
 }
