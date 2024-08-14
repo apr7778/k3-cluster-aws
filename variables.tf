8741f2d80901f28c063f6d9cd3f59089ea9a1b4e
andrew reichek <andrewreichek@andrews-iMac.local>
2024-08-14 10:17:48 -0500

    pushing

--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
-variable "aws_access_key" {
-  description = "AWS access key"
-  type        = string
-}
+# variable "aws_access_key" {
+#   description = "AWS access key"
+#   type        = string
+# }
 
-variable "aws_secret_key" {
-  description = "AWS secret key"
-  type        = string
-  sensitive   = true
-}
+# variable "aws_secret_key" {
+#   description = "AWS secret key"
+#   type        = string
+#   sensitive   = true
+# }
 
 variable "aws_region" {
   description = "AWS region"
